209ad7998938ca53c48dfdcbfb213d60187b416c
daniel sousa <dsousa@adobe.com>
2021-04-19 15:23:56 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[contentHash].js',
+        publicPath: '/'
+    },
+    
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                resolve: {
+                    extensions: ['.js', '.jsx']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HotModuleReplacementPlugin(),
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.[contentHash].css'
+        })
+    ],
+
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, 'dist'),
+        hot: true,
+        port: 8000
+    }
+}
